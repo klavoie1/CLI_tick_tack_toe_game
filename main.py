3006b70713d607d0a0f8d361146cc17add3301d6
Kenneth <kenlavoie19@gmail.com>
2025-04-04 20:37:01 -0400

    Project description

---
+++ main.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------------------
+# This is a Command Line Project, that aims to be a two player
+# Tick-Tack-Toe Game. The first person to go will select either
+# X or O and make a selection based on a common game grid.
+# The next player will then be allowed to place their position on the 
+# grid. This will continue until either someone gets three in a row
+# or the game ends in a tie. 
+#
+# Date Started: 04/04/2025
+# --------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    pass
